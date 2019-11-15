c3df782fbf9187b190e60e82f7103d3166ac6b92
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2019-11-15 18:52:32 +0200

    Initial commit for g1-5

---
+++ demo1/g1-5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace g1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
